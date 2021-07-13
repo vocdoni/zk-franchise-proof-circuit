2efeecc12669279dcf493bd9c782ba80b3c87dc8
arnaucube <root@arnaucube.com>
2021-07-13 13:18:46 +0200

    Update circuit to last design (rm signature & keyKeepers)
    Update circuit to last design (rm signature & keyKeepers), spec can be
    found at: https://github.com/vocdoni/docs/pull/34
    
    Number of constraints of the circuit previous to this PR and with the
    circuit of this PR:
    (When using the old circuit design, I used 2 different versions, one
    that includes the keykeepers and the other without it, to be able to see
    the improvement of just removing the signature verification)
    
    | Circuit | nLvls | census size | KeyKeepers part | Signature verif | The rest | num Constraints |
    | ------- | ----- | ----------- | --------------- | --------------- | -------- | --------------- |
    | Old1    | 4     | 16          | yes             | yes             | yes      | 8215            |
    | Old2    | 4     | 16          | no              | yes             | yes      | 7744            |
    | New     | 4     | 16          | no              | no              | yes      | 2729            |
    |         |       |             |                 |                 |          |                 |
    | Old1    | 20    | 1048576     | yes             | yes             | yes      | 12167           |
    | Old2    | 20    | 1048576     | no              | yes             | yes      | 11696           |
    | New     | 20    | 1048576     | no              | no              | yes      | 6681            |
    |         |       |             |                 |                 |          |                 |
    | Old1    | 32    | 4294967296  | yes             | yes             | yes      | 15131           |
    | Old2    | 32    | 4294967296  | no              | yes             | yes      | 14660           |
    | New     | 32    | 4294967296  | no              | no              | yes      | 9645            |

--- test/go-inputs-generator/census_test.go
+++ test/go-inputs-generator/census_test.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"bytes"
-	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"math/big"
 	"testing"
 
-	"github.com/iden3/go-iden3-crypto/babyjub"
 	"github.com/iden3/go-iden3-crypto/poseidon"
 	"github.com/stretchr/testify/assert"
 	"github.com/vocdoni/arbo"
@@ -17,38 +15,35 @@ import (
 )
 
 func TestCensus(t *testing.T) {
-	testCensus(t, "inputs0.json", 3, 1, 0)
-	testCensus(t, "inputs1.json", 9, 10, 10)
-	testCensus(t, "inputs2.json", 19, 50, 1000)
+	testCensus(t, "inputs0.json", 3, 0)
+	testCensus(t, "inputs1.json", 9, 10)
+	testCensus(t, "inputs2.json", 19, 1000)
 }
 
-func testCensus(t *testing.T, inputsFileName string, nLevels, nMiners, nPaddingLeafs int) {
-	fmt.Printf("%d levels, %d miner, %d padding leafs\n", nLevels, nMiners, nPaddingLeafs)
+func testCensus(t *testing.T, inputsFileName string, nLevels, nPaddingLeafs int) {
+	fmt.Printf("%d levels, %d padding leafs\n", nLevels, nPaddingLeafs)
 
 	// --- User side
 	// -------------
 	// new babyjubjub PrivateKey
-	privKHex := "28156abe7fe2fd433dc9df969286b96666489bac508612d0e16593e944c4f69f"
-	var k babyjub.PrivateKey
-	if _, err := hex.Decode(k[:], []byte(privKHex)); err != nil {
-		panic(err)
-	}
+	secretKeyStr := "3876493977147089964395646989418653640709890493868463039177063670701706079087"
+	secretKey, ok := new(big.Int).SetString(secretKeyStr, 10)
+	assert.True(t, ok)
 
 	// --- Org side
 	// ------------
 	// new CensusTree
-	censusTree, err := arbotree.NewTree("test0", t.TempDir(), nLevels, arbo.HashFunctionBlake2b)
+	censusTree, err := arbotree.NewTree("test0", t.TempDir(), nLevels, arbo.HashFunctionPoseidon)
 	assert.Nil(t, err)
 
-	leafKeyBI, err := poseidon.Hash([]*big.Int{
-		k.Public().X,
-		k.Public().Y,
+	secretKeyHashBI, err := poseidon.Hash([]*big.Int{
+		secretKey,
 	})
 	bLen := arbo.HashFunctionPoseidon.Len()
-	leafKey := arbo.BigIntToBytes(bLen, leafKeyBI)
+	secretKeyHashBytes := arbo.BigIntToBytes(bLen, secretKeyHashBI)
 
 	// add keyHash to CensusMerkleTree
-	err = censusTree.Add(leafKey, []byte{0})
+	err = censusTree.Add(secretKeyHashBytes, []byte{0})
 	assert.Nil(t, err)
 
 	// add extra claims to fill the MerkleTree
@@ -60,7 +55,7 @@ func testCensus(t *testing.T, inputsFileName string, nLevels, nMiners, nPaddingL
 	}
 
 	// get merkleproof
-	proof, err := censusTree.GenProof(leafKey, []byte{0})
+	proof, err := censusTree.GenProof(secretKeyHashBytes, []byte{0})
 	assert.Nil(t, err)
 	siblings, err := arbo.UnpackSiblings(arbo.HashFunctionPoseidon, proof)
 	assert.Nil(t, err)
@@ -80,63 +75,23 @@ func testCensus(t *testing.T, inputsFileName string, nLevels, nMiners, nPaddingL
 
 	// --- User side
 	// -------------
-	// sign vote with PrivateKey
 	vote := big.NewInt(1)
-	voteSig := k.SignPoseidon(vote)
 
 	// compute nullifier
 	electionId := big.NewInt(10)
 	nullifier, err := poseidon.Hash([]*big.Int{
-		babyjub.SkToBigInt(&k),
+		secretKey,
 		electionId,
 	})
-
-	// relayerPublicKey & relayerProof
-	relayerPublicKey := big.NewInt(100)
-	relayerProof, err := poseidon.Hash([]*big.Int{
-		nullifier,
-		relayerPublicKey,
-	})
-
-	// revealKey & commitKey
-	var revealKey []*big.Int
-	var commitKey []*big.Int
-	for i := 0; i < nMiners; i++ {
-		rk := big.NewInt(int64(i))
-		ck, err := poseidon.Hash([]*big.Int{
-			rk,
-		})
-		assert.Nil(t, err)
-
-		revealKey = append(revealKey, rk)
-		commitKey = append(commitKey, ck)
-	}
-	var revealKeyStr []string
-	var commitKeyStr []string
-	for i := 0; i < nMiners; i++ {
-		revealKeyStr = append(revealKeyStr, revealKey[i].String())
-		commitKeyStr = append(commitKeyStr, commitKey[i].String())
-	}
-	jsonRevealKey, err := json.Marshal(revealKeyStr)
-	assert.Nil(t, err)
-	jsonCommitKey, err := json.Marshal(commitKeyStr)
-	assert.Nil(t, err)
 
 	w := bytes.NewBufferString("")
 	fmt.Fprintf(w, "{\n")
 	fmt.Fprintf(w, `	"censusRoot": "%s",`+"\n", arbo.BytesToBigInt(rootBytes[:]))
 	fmt.Fprintf(w, `	"censusSiblings": %s,`+"\n", jsonSiblings) // TMP
-	fmt.Fprintf(w, `	"privateKey": "%s",`+"\n", babyjub.SkToBigInt(&k))
-	fmt.Fprintf(w, `	"voteSigS": "%s",`+"\n", voteSig.S.String())
-	fmt.Fprintf(w, `	"voteSigR8x": "%s",`+"\n", voteSig.R8.X.String())
-	fmt.Fprintf(w, `	"voteSigR8y": "%s",`+"\n", voteSig.R8.Y.String())
+	fmt.Fprintf(w, `	"secretKey": "%s",`+"\n", secretKey.String())
 	fmt.Fprintf(w, `	"voteValue": "%s",`+"\n", vote.String())
 	fmt.Fprintf(w, `	"electionId": "%s",`+"\n", electionId.String())
-	fmt.Fprintf(w, `	"nullifier": "%s",`+"\n", nullifier.String())
-	fmt.Fprintf(w, `	"relayerPublicKey": "%s",`+"\n", relayerPublicKey.String())
-	fmt.Fprintf(w, `	"relayerProof": "%s",`+"\n", relayerProof.String())
-	fmt.Fprintf(w, `	"revealKey": %s,`+"\n", jsonRevealKey)
-	fmt.Fprintf(w, `	"commitKey": %s`+"\n", jsonCommitKey)
+	fmt.Fprintf(w, `	"nullifier": "%s"`+"\n", nullifier.String())
 	fmt.Fprintln(w, "}")
 
 	err = ioutil.WriteFile(inputsFileName, w.Bytes(), 0600)
--- test/lib.test.ts
+++ test/lib.test.ts
@@ -1,92 +1,65 @@
 const path = require("path");
 const tester = require("circom").tester;
-// const chai = require("chai");
-// const assert = chai.assert;
 
 const { Election, Voter } = require("../src/franchise");
 
 describe("lib test", function () {
     this.timeout(200000);
 
-    it("Test Census 3lvl 1+0 claims, correct voter proof & incorrect revealKeys", async () => {
-       const voter = new Voter("0001020304050607080900010203040506070809000102030405060708090021");
-      
-        const election = new Election(/* electionid */ 1, /* levels */ 4, /* authorities */ 1);
-        await election.addCensus(voter.getPublicKeyHash());
+    it("Test Census 3lvl", async () => {
+        const voter = new Voter("3876493977147089964395646989418653640709890493868463039177063670701706079087");
 
-        const voterData = await election.voterData(voter.getPublicKeyHash());
+        const election = new Election(/* electionid */ 1, /* levels */ 4);
+        await election.addCensus(voter.getSecretKeyHash());
+
+        const voterData = await election.voterData(voter.getSecretKeyHash());
         const voteValue = BigInt(2);
-        const relayerPublicKey = BigInt(1); 
-	let input = voter.vote(voterData, voteValue, relayerPublicKey);
+        let input = voter.vote(voterData, voteValue);
 
         const circuit = await tester(
             path.join(__dirname, "circuits", "census4lvl.circom"),
             {reduceConstraints: false}
         );
- 
+
         const witness = await circuit.calculateWitness(input);
         await circuit.checkConstraints(witness);
     }),
 
-    it("Test Census 3lvl 1+0 claims, fake-voter proof & correct revealKeys", async () => {
-        const voter = new Voter("0001020304050607080900010203040506070809000102030405060708090021");
-      
-        const election = new Election(/* electionid */ 1, /* levels */ 4, /* authorities */ 1);
-        await election.addCensus(voter.getPublicKeyHash());
+    it("Test Census 9lvl", async () => {
+        const voter = new Voter("3876493977147089964395646989418653640709890493868463039177063670701706079087");
 
-        const voterData = await election.voterData(voter.getPublicKeyHash());
+        const election = new Election(/* electionid */ 1, /* levels */ 10);
+        await election.addCensus(voter.getSecretKeyHash());
+
+        const voterData = await election.voterData(voter.getSecretKeyHash());
         const voteValue = BigInt(2);
-        const relayerPublicKey = BigInt(1); 
-	let input = voter.vote(voterData, voteValue, relayerPublicKey);
-	input.voteSigS++;
-	input.revealKey = election.revealKey;
-
-        const circuit = await tester(
-            path.join(__dirname, "circuits", "census4lvl.circom"),
-            {reduceConstraints: false}
-        );
- 
-        const witness = await circuit.calculateWitness(input);
-        await circuit.checkConstraints(witness);
-    });
-
-    it("Test Census 9lvl 1+10 claims, correct voter proof & incorrect revealKeys", async () => {
-        const voter = new Voter("0001020304050607080900010203040506070809000102030405060708090021");
-      
-        const election = new Election(/* electionid */ 1, /* levels */ 10, /* authorities */ 10);
-        await election.addCensus(voter.getPublicKeyHash());
-
-        const voterData = await election.voterData(voter.getPublicKeyHash());
-        const voteValue = BigInt(2);
-        const relayerPublicKey = BigInt(1); 
-	let input = voter.vote(voterData, voteValue, relayerPublicKey);
+        let input = voter.vote(voterData, voteValue);
 
         const circuit = await tester(
             path.join(__dirname, "circuits", "census10lvl.circom"),
             {reduceConstraints: false}
         );
- 
+
         const witness = await circuit.calculateWitness(input);
         await circuit.checkConstraints(witness);
     });
 
 
-    it("Test Census 19lvl 1+50 claims, correct voter proof & incorrect revealKeys", async () => {
-        const voter = new Voter("0001020304050607080900010203040506070809000102030405060708090021");
-      
-        const election = new Election(/* electionid */ 1, /* levels */ 20, /* authorities */ 50);
-        await election.addCensus(voter.getPublicKeyHash());
+    it("Test Census 19lvl", async () => {
+        const voter = new Voter("3876493977147089964395646989418653640709890493868463039177063670701706079087");
 
-        const voterData = await election.voterData(voter.getPublicKeyHash());
+        const election = new Election(/* electionid */ 1, /* levels */ 20);
+        await election.addCensus(voter.getSecretKeyHash());
+
+        const voterData = await election.voterData(voter.getSecretKeyHash());
         const voteValue = BigInt(2);
-        const relayerPublicKey = BigInt(1); 
-	let input = voter.vote(voterData, voteValue, relayerPublicKey);
+        let input = voter.vote(voterData, voteValue);
 
         const circuit = await tester(
             path.join(__dirname, "circuits", "census20lvl.circom"),
             {reduceConstraints: false}
         );
- 
+
         const witness = await circuit.calculateWitness(input);
         await circuit.checkConstraints(witness);
     });
