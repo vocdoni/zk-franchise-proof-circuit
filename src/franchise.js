8529cc3f7e571f0e39cf3b4d8718ae09738c5f19
arnaucube <root@arnaucube.com>
2021-07-13 13:18:46 +0200

    Update circuit to last design (rm signature & keyKeepers)
    Update circuit to last design (rm signature & keyKeepers), spec can be
    found at: https://github.com/vocdoni/docs/pull/34

--- src/franchise.js
+++ src/franchise.js
@@ -1,93 +1,59 @@
 const bigInt = require("snarkjs").bigInt;
 const { assert } = require("chai");
-const createBlakeHash = require("blake-hash");
-const { babyJub, eddsa, smt, poseidon } = require("circomlib");
-const ffutils = require("ffjavascript").utils;
-const Scalar = require("ffjavascript").Scalar;
-const EC = require("elliptic").ec;
-const ec = new EC("secp256k1");
+const { smt, poseidon } = require("circomlib");
 
 class Election {
-    constructor(electionId, levels, authorities) {
-        this.electionId = electionId;
-	this.levels = levels;
-        this.tree = null;
-	this.commitKey = []
-        this.revealKey = []
-        for (let n=0;n<authorities;n++) {
-            const raw = BigInt(1+n);
-            this.revealKey.push(raw);
-	    this.commitKey.push(poseidon([raw]));
-        }
-    }
-    async addCensus(publicKeyHash) {
-        if (this.tree === null) {
-            this.tree = await smt.newMemEmptyTrie();
-        }
-        await this.tree.insert(publicKeyHash, 0);
-    }
-    async voterData(publicKeyHash) {
-        const res = await this.tree.find(publicKeyHash);
-        assert(res.found);
-        let siblings = res.siblings;
-        while (siblings.length < this.levels) siblings.push(BigInt(0));
-        
-        return {
-	    electionId : this.electionId,
-            root: this.tree.root,
-            siblings: siblings,
-            commitKey : this.commitKey,
-        };
-    }
+   constructor(electionId, levels) {
+      this.electionId = electionId;
+      this.levels = levels;
+      this.tree = null;
+   }
+   async addCensus(secretKeyHash) {
+      if (this.tree === null) {
+         this.tree = await smt.newMemEmptyTrie();
+      }
+      await this.tree.insert(secretKeyHash, 0);
+   }
+   async voterData(secretKeyHash) {
+      const res = await this.tree.find(secretKeyHash);
+      assert(res.found);
+      let siblings = res.siblings;
+      while (siblings.length < this.levels) siblings.push(BigInt(0));
+
+      return {
+         electionId : this.electionId,
+         root: this.tree.root,
+         siblings: siblings,
+      };
+   }
 }
 
 class Voter {
-    constructor(rawpvkHex) {
-       const rawpvk = Buffer.from(rawpvkHex, "hex");
-       const rawpvkHash = eddsa.pruneBuffer(createBlakeHash("blake512").update(rawpvk).digest().slice(0, 32));
-       const pvk = Scalar.shr(ffutils.leBuff2int(rawpvkHash),3);
-       const A = babyJub.mulPointEscalar(babyJub.Base8, pvk);
-       this.key = { rawpvk , pvk , pbk : { x: A[0], y: A[1] } }
+   constructor(secretKey) {
+      this.key = { secretKey }
    }
 
-    getPublicKeyHash() {
-        return poseidon([this.key.pbk.x, this.key.pbk.y]);
-    }
+   getSecretKeyHash() {
+      return poseidon([this.key.secretKey]);
+   }
 
-    vote(voterData, voteValue, relayerPublicKey) {
-        const nullifier = poseidon([this.key.pvk, voterData.electionId]);
-        const signature = eddsa.signPoseidon(this.key.rawpvk, voteValue);
-	const relayerProof = poseidon([nullifier, relayerPublicKey])
+   vote(voterData, voteValue) {
+      const nullifier = poseidon([this.key.secretKey, voterData.electionId]);
 
-	let revealKey = [] 
-        for (let n=0;n<voterData.commitKey.length;n++) {
-            revealKey.push(BigInt(0));
-        }
+      return {
+         censusRoot: voterData.root,
+         censusSiblings: voterData.siblings,
+         secretKey : BigInt(this.key.secretKey),
 
-        return {
-            censusRoot: voterData.root,
-            censusSiblings: voterData.siblings,
-	    privateKey : this.key.pvk,
+         voteValue,
 
-            voteSigS: signature.S,
-            voteSigR8x: signature.R8[0],
-            voteSigR8y: signature.R8[1],
- 
-            voteValue,
-
-	    electionId: BigInt(voterData.electionId),
-            nullifier,
-           
-            relayerPublicKey,
-	    relayerProof,
-
-	    revealKey,
-	    commitKey : voterData.commitKey
-        }
-    }
+         electionId: BigInt(voterData.electionId),
+         nullifier,
+      }
+   }
 }
 
 module.exports = {
-    Election,
-    Voter
+   Election,
+   Voter
 }
--- test/go-inputs.test.ts
+++ test/go-inputs.test.ts
@@ -7,7 +7,7 @@ const tester = require("circom").tester;
 describe("Check inputs generated from go test vectors", function () {
     this.timeout(200000);
 
-    it("Test Census 3lvl 1+0 claims, correct voter proof & incorrect revealKeys", async () => {
+    it("Test Census 3lvl", async () => {
         const circuit = await tester(
             path.join(__dirname, "circuits", "census4lvl.circom"),
             {reduceConstraints: false}
@@ -21,7 +21,7 @@ describe("Check inputs generated from go test vectors", function () {
         await circuit.checkConstraints(witness);
     });
     
-    it("Test Census 9lvl 1+10 claims, correct voter proof & incorrect revealKeys", async () => {
+    it("Test Census 9lvl", async () => {
         const circuit = await tester(
             path.join(__dirname, "circuits", "census10lvl.circom")
         );
@@ -34,7 +34,7 @@ describe("Check inputs generated from go test vectors", function () {
         await circuit.checkConstraints(witness);
     });
     
-    it("Test Census 19lvl 1+100 claims, correct voter proof & incorrect revealKeys", async () => {
+    it("Test Census 19lvl", async () => {
         const circuit = await tester(
             path.join(__dirname, "circuits", "census20lvl.circom")
         );
@@ -44,30 +44,6 @@ describe("Check inputs generated from go test vectors", function () {
         let inputs = JSON.parse(rawdata);
     
         const witness = await circuit.calculateWitness(inputs);
-        await circuit.checkConstraints(witness);
-    });
-
-    it("Test Census 3lvl 1+0 claims, fake-voter proof & correct revealKeys", async () => {
-        const circuit = await tester(
-            path.join(__dirname, "circuits", "census4lvl.circom")
-        );
-    
-        // using incorrect voter proof, but correct revealKeys
-        const witness = await circuit.calculateWitness({
-            censusRoot: "0",
-            censusSiblings: ["0","0","0","0"],
-            privateKey: "0",
-            voteSigS: "0",
-            voteSigR8x: "0",
-            voteSigR8y: "0",
-            voteValue: "1",
-            electionId: "10",
-            nullifier: "0",
-            relayerPublicKey: "100",
-            relayerProof: "21349690342514405503176665977362532634490340702670001813783738965751319356478",
-            revealKey: ["0"],
-            commitKey: ["19014214495641488759237505126948346942972912379615652741039992445865937985820"]
-        });
         await circuit.checkConstraints(witness);
     });
 });
